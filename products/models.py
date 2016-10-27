b4165b9cd4bbc6ef897e800b12a226e53638d036
Hugo <hugo-ensc@outlook.com>
2016-10-27 00:45:13 -0500

    Modelos aun no terminados

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Product(models.Model):
+	name = models.CharField(max_length=150)
+	presentation = models.CharField(max_length=100)
+	availability = models.IntegerField()
+	price = models.FloatField()
+
+	
