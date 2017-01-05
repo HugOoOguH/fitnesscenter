578f42d11ea5387c6afb9b54d74583e3a8498e1e
IvanMSP <ivanlopz11@gmail.com>
2017-01-04 21:43:40 -0600

    Se corrigen models de Products y se cambia app por SHOP_PRODUCT

--- fitnesscenter/settings.py
+++ fitnesscenter/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'clients',
     'monthlypayments',
-    'products',
+    'shop_product',
     'class',
     'visits',
     'home',
@@ -127,5 +127,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 MEDIA_URL = '/media/' 
