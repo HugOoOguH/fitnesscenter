13921f4243eae34595fa5c7dc4cbce99a729c813
IvanMSP <ivanlopz11@gmail.com>
2017-01-05 19:13:03 -0600

    Se agrego url de shop_product

--- fitnesscenter/urls.py
+++ fitnesscenter/urls.py
@@ -18,12 +18,15 @@ from django.contrib import admin
 from django.views.static import serve
 from django.conf import settings
 from home import urls as urlsHome
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(urlsHome)),
-    url(
-    	regex=r'^media/(?P<path>.*)$',
-    	view=serve,
-    	kwargs={'document_root':settings.MEDIA_ROOT}),
+    url(r'^shop/', include('shop_product.urls',namespace='shop_product')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
